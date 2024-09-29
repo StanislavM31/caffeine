68daa2e912993f7a17249ba71ea9b626f3c4e848
Stanislav Minko <minkostas@gmail.com>
2024-09-30 00:00:01 +0300

    :feature/header

--- caffeine/src/components/Header/Header.tsx
+++ caffeine/src/components/Header/Header.tsx
@@ -1,61 +1,62 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    max-width: 1220px;  
+  max-width: 1220px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2% 0;
+  margin: auto;
+  h1 {
+    font-family: Clicker Script;
+    font-size: 35px;
+    font-weight: 400;
+    color: #ffffff;
+  }
+
+  button {
+    display: block;
+    color: #ffffff;
+  }
+
+  .btn_active {
+    background: #f9c06a;
+    padding: 14px 27px;
+    border-radius: 24px;
+    color: #1e1e1e;
+  }
+
+  .container {
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 2% 0;
-    margin: auto;
-    h1 {
-        font-family: Clicker Script;
-        font-size: 35px;
-        font-weight: 400;
-        color: #FFFFFF;
-    }
+    gap: 35px;
+  }
 
-    button {
-        display: block;
-        color: #FFFFFF;
-    }
+  .navigation {
+    display: flex;
+    gap: 60px;
 
-    .btn_active {
-        background: #F9C06A;
-        padding: 14px 27px;
-        border-radius: 24px;
-        color: #1E1E1E;
+    p {
+      color: #ffffff;
     }
-
-    .container {
-        display: flex;
-        gap: 35px;
-    }
-
-    .navigation {
-        display: flex;
-        gap: 60px;
-
-        p {
-            color: #FFFFFF;
-        }
-    }
-`
+  }
+`;
 const Header = () => {
-    const nav: string[] = ['Home', 'Menu', 'About Us', 'Contact Us']
+  const nav: string[] = ["Home", "Menu", "About Us", "Contact Us"];
 
-    return <>
-        <Wrapper>
-            <h1>Caffeine</h1>
-            <div className="navigation">
-                {nav.map((el, i) => <p key={i}>{el}</p>)}
-            </div>
+  return (
+    <Wrapper>
+    <h1>Caffeine</h1>
+    <div className="navigation">
+      {nav.map((el, i) => (
+        <p key={i}>{el}</p>
+      ))}
+    </div>
 
-            <div className="container">
-                <button>Sign In</button>
-                <button className="btn_active">Sign Up</button>
-            </div>
-
-        </Wrapper>
-    </>
-}
-export default Header
+    <div className="container">
+      <button>Sign In</button>
+      <button className="btn_active">Sign Up</button>
+    </div>
+  </Wrapper>
+  )
+};
+export default Header;
