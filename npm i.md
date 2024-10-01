cd7f3f51230587ddd67f54f535aac53d0f70c638
Stanislav Minko <minkostas@gmail.com>
2024-10-01 22:51:44 +0300

    super_commit

--- npm i.md
+++ npm i.md
@@ -3,3 +3,6 @@ npm install styled-components
 npm install @vitejs/plugin-react
 npm install @types/react @types/react-dom
 npm install --save-dev @types/react @types/react-dom
+
+npm i husky
+npx husky init
---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,64 @@
+/* import styled from "styled-components" */
+/* import { useState } from "react";
+
+
+
+
+const sliderObj = [
+    {
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset.....`,
+      name: `Jonny Thomas`,
+      profession: `Project Manager`
+    },
+    {
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis enim doloremque perspiciatis nostrum inventore consequuntur dicta exercitationem! Optio debitis maxime explicabo ab ad, fugiat voluptas?`,
+      name: `Jon Smith`,
+      profession: `Front`
+    },
+    {
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis soluta nam quia minima, fugiat totam ratione distinctio voluptatum iure consequatur quisquam dicta alias reiciendis placeat harum architecto neque cum pariatur.`,
+      name: `Anton`,
+      profession: `Dev`
+    },
+    {
+      description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam dolorem officia veniam dolor nobis voluptates necessitatibus vero, porro nulla labore.`,
+      name: `Jonny Thomas`,
+      profession: `HR`
+    },
+    {
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa adipisci harum, tempora perferendis amet delectus?`,
+      name: `Thomas`,
+      profession: `Project Manager`
+    },
+  ]
+
+const Wrapper = styled.div`
+    max-width: 980px; 
+    height: 524px, 
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2% 0;
+    margin: auto;
+    }
+` */
+
+/* const Slider = () => {
+    const [count, SetCount] = useState(1);
+
+    const nextSlide = () => {
+        count === sliderObj.length-1 ? SetCount(0):SetCount(count+1);
+    }
+    const lastSlide = ()=>{
+        count === 0? SetCount(sliderObj.length-1):SetCount(count-1);
+    }
+
+    return <>
+        <Wrapper>
+            <div>
+                
+            </div>
+        </Wrapper>
+    </>
+}
+export default Slider */
